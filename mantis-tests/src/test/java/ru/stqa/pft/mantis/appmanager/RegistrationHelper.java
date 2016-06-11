df677d8ce25828903ab083aae0b260696051b664
Gavrilova <gavrilova.irina@gmail.com>
2016-06-11 10:32:35 -0500

    Getting mails to MailServer.
    Lecture 8, module 6.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,21 +1,29 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
 /**
  * Created by irinagavrilova on 5/31/16.
  */
-public class RegistrationHelper { //этому помощнику браузер нужен
-
-  private final ApplicationManager app;
-  private WebDriver wd;
+public class RegistrationHelper extends HelperBase { //этому помощнику браузер нужен
 
   public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
+    super(app);
    // wd = app.wd; //просим ссылку на driver у ApplicationManager //меняем на ленивую инициализацию
-    wd = app.getDriver(); //метод будет инициализировать driver в момент первого обращения
+    // wd = app.getDriver();метод будет инициализировать driver в момент первого обращения
   }
   public void start (String username, String email) {
-  wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+    type(By.name("username"), username);
+    type(By.name("email"), email);
+    click(By.cssSelector("input[value='Signup']"));
+
+  }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    click(By.cssSelector("input[value='Update User']"));
   }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,7 +1,17 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
 import static ru.stqa.pft.mantis.tests.TestBase.app;
 
 /**
@@ -9,8 +19,33 @@ import static ru.stqa.pft.mantis.tests.TestBase.app;
  */
 public class RegistrationTests extends TestBase {
 
+  @BeforeMethod
+  public void startMailServer() {
+    app.mail().start();
+  }
+
   @Test
-  public void testRegistration() {
-    app.registration().start("user1", "user1@localhost.localdomain");
+  public void testRegistration() throws InterruptedException, MessagingException, IOException {
+    long now = System.currentTimeMillis();
+    String user = String.format("user%s", now);
+    String password = "password";
+    String email = String.format("user%s@localhost.localdomain", now);
+    app.registration().start(user, email);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 50000);
+    String confirmationLink = findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, password);
+    assertTrue(app.newSession().login(user, password));
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    regex.getText(mailMessage.text);
+    return regex.getText(mailMessage.text);
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void stopMailserver(){
+    app.mail().stop();
   }
 }
