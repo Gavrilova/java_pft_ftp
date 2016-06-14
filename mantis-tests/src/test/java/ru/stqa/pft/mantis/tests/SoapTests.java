9bee4b28bfc9076a4e8b98d7b4b8a9329dda1a5e
Gavrilova <gavrilova.irina@gmail.com>
2016-06-14 00:19:05 -0500

    Creating bug reports in bugtracker Mantis
    Lecture 9, module 3. Part 2.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 import java.util.Set;
+
+import static org.testng.Assert.*;
 
 /**
  * Created by irinagavrilova on 6/13/2016.
@@ -21,5 +25,13 @@ public class SoapTests extends TestBase {
       System.out.println(project.getName());
     }
   }
-  
+
+  @Test
+  public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+    Set<Project> projects = app.soap().getProjects();
+    Issue issue = new Issue().withSummary("Test issue").withDescription("Test issue description")
+            .withProject(projects.iterator().next());
+    Issue created = app.soap().addIssue(issue);
+    assertEquals(issue.getSummary(), created.getSummary());
+  }
 }
